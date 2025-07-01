4e160792c95e989e910c80c8eb30d2172e981e99
madil002 <muabdul.adil@gmail.com>
2025-07-01 17:18:47 +0100

    added environmental variables

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask
 import redis
 
 app = Flask(__name__)
-r = redis.Redis(host='redis', port=6379, db=0)
+REDIS_HOST = os.getenv('REDIS_HOST', 'redis')
+REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
+r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
 
 @app.route('/')
 def welcome():
